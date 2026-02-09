3d8cfd15c689f72ab688efcbb45d65b00e7c60cc
JeeJ <gustavogomescoding@gmail.com>
2026-02-09 11:16:38 -0300

    .

--- clone-instagram/src/App.jsx
+++ clone-instagram/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+
+import logo from "/photos/logo.png";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="container-fluid">
+      <nav className="navbar">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="https://www.instagram.com/">
+            <img src={logo} width={100} height={44} alt="Logo" />
+          </a>
+          <div className="d-flex align-items-center gap-2">
+            <i className="bi bi-plus-lg fs-2"></i>
+            <i className="bi bi-suit-heart fs-4"></i>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-export default App
+export default App;
