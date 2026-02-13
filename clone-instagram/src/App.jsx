6cbd66d798f4ea46ed5c6d1d43081bba269d6333
JeeJ <gustavogomescoding@gmail.com>
2026-02-13 10:53:20 -0300

    incremento-post

--- clone-instagram/src/App.jsx
+++ clone-instagram/src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import logo from "/photos/logo.png";
 import userIconStory from "/photos/user-icon-story.jpg";
 import iconStory from "/photos/icon-story.jpg";
+import userIconPost1 from "/photos/user-icon-post-1.jpg";
 import post1 from "/photos/post-1.jpg";
 import "./App.css";
 
@@ -91,11 +92,23 @@ const App = () => {
         </div>
       </div>
 
-      <div className="row">
+      <div className="post-container row mb-2">
         <div className="col-12">
-          <div className="row">
+          <div className="post-header row">
             <div className="col-6">
-              <span>amorabernesse</span>
+              <div className="post-user-info">
+                <div className="post-circle with-story">
+                  <div className="post-circle-whitespace">
+                    <img
+                      className="post-user-icon"
+                      src={userIconPost1}
+                      alt="icon-user-post"
+                    />
+                  </div>
+                </div>
+
+                <span className="post-username">amorabernesse</span>
+              </div>
             </div>
             <div className="col-6">
               <div className="d-flex justify-content-end">
@@ -103,17 +116,82 @@ const App = () => {
               </div>
             </div>
           </div>
-          <div className="row">
+          <div className="post-image row">
             <div className="col-12">
-              <img className="img-fluid" src={post1} alt="" />
+              <img className="img-fluid" src={post1} alt="post-image" />
             </div>
           </div>
-          <div className="row">
-            <div className="col-6"></div>
-            <div className="col-6"></div>
+          <div className="post-action-row row">
+            <div className="col-6">
+              <div className="d-flex gap-2">
+                <i class="bi bi-suit-heart"></i>
+                <i class="bi bi-chat"></i>
+                <i class="bi bi-send"></i>
+              </div>
+            </div>
+            <div className="col-6 d-flex justify-content-end">
+              <i class="bi bi-bookmark"></i>
+            </div>
           </div>
-          <div className="row">
-            <div className="col-12"></div>
+          <div className="post-description row">
+            <div className="col-12">
+              <div>
+                <span className="fw-semibold me-1">amorabernesse</span>
+                Dia de passear com essa princesa
+              </div>
+              <span className="past-time text-secondary">há 15 minutos</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="post-container row mb-2">
+        <div className="col-12">
+          <div className="post-header row">
+            <div className="col-6">
+              <div className="post-user-info">
+                <div className="post-circle">
+                  <img
+                    className="post-user-icon"
+                    src={userIconPost1}
+                    alt="icon-user-post"
+                  />
+                </div>
+
+                <span className="post-username">amorabernesse</span>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="d-flex justify-content-end">
+                <i class="bi bi-three-dots"></i>
+              </div>
+            </div>
+          </div>
+          <div className="post-image row">
+            <div className="col-12">
+              <img className="img-fluid" src={post1} alt="post-image" />
+            </div>
+          </div>
+          <div className="post-action-row row">
+            <div className="col-6">
+              <div className="d-flex gap-2">
+                <i class="bi bi-suit-heart"></i>
+                <i class="bi bi-chat"></i>
+                <i class="bi bi-send"></i>
+              </div>
+            </div>
+            <div className="col-6 d-flex justify-content-end">
+              <i class="bi bi-bookmark"></i>
+            </div>
+          </div>
+          <div className="post-description row">
+            <div className="col-12">
+              <div>
+                <span className="fw-semibold me-1">amorabernesse</span>
+                Dia de passear com essa princesa
+              </div>
+              <span className="past-time text-secondary">há 15 minutos</span>
+            </div>
           </div>
         </div>
       </div>
