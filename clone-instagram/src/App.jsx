9900f17ab0e0e1be8fd037b6342650e3c0e5a3ef
Gustavo <gustavogomes1108@gmail.com>
2026-02-21 10:36:36 -0300

    add-follow-button

--- clone-instagram/src/App.jsx
+++ clone-instagram/src/App.jsx
@@ -112,8 +112,95 @@ const App = () => {
                     <span className="post-username">amorabernesse</span>
                   </div>
                 </div>
+                <div className="col-6 d-flex justify-content-end align-items-center">
+                  <div className="d-flex gap-2">
+                    <span className="post-user-follow">Follow</span>
+                    <i className="bi bi-three-dots"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="post-image">
+              <div className="row">
+                <div className="col-12">
+                  <img className="img-fluid" src={post1} alt="post-image" />
+                </div>
+              </div>
+            </div>
+            <div className="post-action-row">
+              <div className="row">
                 <div className="col-6">
-                  <div className="d-flex justify-content-end">
+                  <div className="d-flex gap-2">
+                    <i className="bi bi-suit-heart"></i>
+                    <i className="bi bi-chat"></i>
+                    <i className="bi bi-send"></i>
+                  </div>
+                </div>
+                <div className="col-6 d-flex justify-content-end">
+                  <i className="bi bi-bookmark"></i>
+                </div>
+              </div>
+            </div>
+            <div className="post-description">
+              <div className="row">
+                <div className="col-12">
+                  <div className="description-text">
+                    <span className="fw-semibold me-1">amorabernesse</span>
+                    Alvin, Simon e Teodoro foram indiciados pela polícia pelos
+                    crimes de estelionato e lavagem de dinheiro, segundo
+                    informações divulgadas na manhã deste domingo (15). De
+                    acordo com as investigações, os três irmãos são suspeitos de
+                    envolvimento em um esquema financeiro que teria movimentado
+                    grandes quantias por meio de transações consideradas
+                    fraudulentas. As autoridades afirmam que o inquérito reuniu
+                    elementos suficientes para apontar a participação direta do
+                    trio nos crimes. O caso agora será encaminhado ao Ministério
+                    Público, que deverá analisar as provas e decidir se
+                    apresenta denúncia formal à Justiça. Até o momento, a defesa
+                    dos envolvidos não se manifestou.
+                  </div>
+
+                  <input
+                    type="checkbox"
+                    id="expand-text-checkbox"
+                    className="expand-text-checkbox"
+                  />
+                  <label
+                    htmlFor="expand-text-checkbox"
+                    className="expand-text text-secondary"
+                  >
+                    Mais...
+                  </label>
+                  <div className="text-secondary">há 15 minutos</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="post-container mb-2">
+        <div className="row">
+          <div className="col-12">
+            <div className="post-header">
+              <div className="row">
+                <div className="col-6">
+                  <div className="post-user-info">
+                    <div className="post-circle">
+                      <img
+                        className="post-user-icon"
+                        src={userIconPost1}
+                        alt="icon-user-post"
+                      />
+                    </div>
+
+                    <span className="post-username">amorabernesse</span>
+                  </div>
+                </div>
+                <div className="col-6 d-flex justify-content-end align-items-center">
+                  <div className="d-flex gap-2">
+                    <span className="post-user-follow">Follow</span>
                     <i className="bi bi-three-dots"></i>
                   </div>
                 </div>
